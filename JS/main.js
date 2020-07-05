75cfb381fe4647e8d918fcfc27b8549756b66064
whulizheng <whulizheng@outlook.com>
2020-07-05 12:01:20 +0800

    嘤嘤嘤

---
+++ JS/main.js
@@ -0,0 +1,133 @@
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
+
+	// Fancybox
+	$('.work-box').fancybox();
+
+	// Flexslider
+	$('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
+
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
+
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
+
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
+	 
+//Slider
+ 
+        var time = 7; // time in seconds
+
+        var $progressBar,
+            $bar,
+            $elem,
+            isPause,
+            tick,
+            percentTime;
+
+        //Init the carousel
+        $("#main-slider").find('.owl-carousel').owlCarousel({
+            slideSpeed: 500,
+            paginationSpeed: 500,
+            singleItem: true,
+            navigation: true,
+            navigationText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],
+            afterInit: progressBar,
+            afterMove: moved,
+            startDragging: pauseOnDragging,
+            //autoHeight : true,
+            transitionStyle: "fade" //fadeUp fade goDown backSlide
+
+        });
+ 
+        function progressBar(elem) {
+            $elem = elem; 
+            buildProgressBar(); 
+            start();
+        }
+
+        //create 
+        function buildProgressBar() {
+            $progressBar = $("<div>", {
+                id: "progressBar"
+            });
+            $bar = $("<div>", {
+                id: "bar"
+            });
+            $progressBar.append($bar).appendTo($elem);
+        }
+
+        function start() { 
+            percentTime = 0;
+            isPause = false; 
+            tick = setInterval(interval, 10);
+        };
+
+        function interval() {
+            if (isPause === false) {
+                percentTime += 1 / time;
+                $bar.css({
+                    width: percentTime + "%"
+                }); 
+                if (percentTime >= 100) { 
+                    $elem.trigger('http://demo.qfpffmp.cn/cssthemes6/wait-28-lancer/js/owl.next')
+                }
+            }
+        }
+
+        //pause while dragging 
+        function pauseOnDragging() {
+            isPause = true;
+        }
+
+        //moved callback
+        function moved() {
+            //clear interval
+            clearTimeout(tick);
+            //start again
+            start();
+        }
+    });
